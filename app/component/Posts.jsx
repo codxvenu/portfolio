0cff96e7633d4ab826719df9336cdad9a058be02
codx_venu <vasulallu09@gmail.com>
2026-03-23 21:28:29 +0530

    minor px issue fixed

--- app/component/Posts.jsx
+++ app/component/Posts.jsx
@@ -60,7 +60,7 @@ const Casoul = ({postz,activeThread = false , action=true}) => {
                   </div>
                 )}
                 {post.image && (
-                  <Image alt='image' width={100} height={192} className="mt-3 w-full! h-48! bg-neutral-800 rounded-2xl object-cover" src={`/${post.image}`} />
+                  <Image alt='image' width={600} height={192} className="mt-3 w-full! h-48! bg-neutral-800 rounded-2xl object-cover" src={`/${post.image}`} />
                 )}
                 {post.threadOf && (
                   <div className="mt-3 border border-neutral-800 rounded-xl p-3">
