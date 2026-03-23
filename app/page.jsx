6cc580c82115cc491f24b511f385495abfca264f
codxvenu <158077441+codxvenu@users.noreply.github.com>
2026-03-23 13:41:06 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ export default function XMobileExactClonePortfolio() {
       
       <HomeHeader/>
         <Posts
-          postz={posts.filter(i=> !i.threadId || !i.threadTitle)}
+          postz={posts.filter(i=> !i.threadId && !i.threadTitle)}
         />
        
       </div>
